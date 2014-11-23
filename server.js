e7277f54b8306645eac65609179dc6a3248cc29d
Luis Carlos Cruz Carballo <lcruzc@linkux-it.com>
2014-11-23 19:46:05 -0400

    Improve login

--- server.js
+++ server.js
@@ -110,6 +110,12 @@
 
   app.use(express.csrf());
 
+  app.use(function(req, res, next) {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    res.locals.csrftoken = req.csrfToken();
+    next();
+  });
+
   app.use(gzippo.compress());
 
   app.disable("x-powered-by");
