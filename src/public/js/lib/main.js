af4aa7add8fcc1f83930726fd69979336a08a9a1
Luis Carlos Cruz Carballo <lcruzc@linkux-it.com>
2015-05-03 20:22:54 -0400

    Fix some bugs and add share feature

--- src/public/js/lib/main.js
+++ src/public/js/lib/main.js
@@ -1,3 +1,20 @@
+function openPopup() {
+  var width  = 575,
+      height = 400,
+      left   = ($(window).width()  - width)  / 2,
+      top    = ($(window).height() - height) / 2,
+      url    = this.href,
+      opts   = 'status=1' +
+               ',width='  + width  +
+               ',height=' + height +
+               ',top='    + top    +
+               ',left='   + left;
+
+    window.open(url, $(this).data('title'), opts);
+
+    return false;
+};
+
 $(document).ready(function(){
 	var current_tab = $('.nav-step.active > a');
 	$('.nav.nav-3doers > li > a').mouseover( function(){
@@ -10,4 +27,6 @@ $(document).ready(function(){
     $(href).removeClass('active in');
     $(current_tab.attr('href')).addClass('active in');
   });
+
+  $('.social-popup').click(openPopup);
 });
